9ca03c790a8e7a36e9f7fa15a43d44719eb28c7c
Ethan Mick <ethan@ethanmick.com>
2023-11-06 14:56:35 -0500

    fix: Change subnet count to 1
    Wasn't using the additional subnets. Removed for simplicity. This isn't
    a high availability application. In fact, it's literally the opposite.
    How low availability can we go.

--- variables.tf
+++ variables.tf
@@ -6,11 +6,11 @@ variable "aws_region" {
 variable "public_subnet_cidrs" {
   type        = list(string)
   description = "Public Subnet CIDR values"
-  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  default     = ["10.0.1.0/24"]
 }
 
 variable "availability_zones" {
   type        = list(string)
   description = "Availability Zones"
-  default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  default     = ["us-east-1a"]
 }
